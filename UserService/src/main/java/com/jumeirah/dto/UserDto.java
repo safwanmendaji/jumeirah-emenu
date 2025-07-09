32e846c215aaf74bdebd612866e9de2c7ab5296f
safwanmendaji <mendajisafwan@gmail.com>
2025-07-09 11:06:26 +0530

    test

--- UserService/src/main/java/com/jumeirah/dto/UserDto.java
+++ UserService/src/main/java/com/jumeirah/dto/UserDto.java
@@ -8,14 +8,6 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserDto {
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
     private UUID id;
     private String name;
     private String email;
